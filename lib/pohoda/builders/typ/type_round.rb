9e30820908cbadc5b52ae782922bc0915cb9f576
Martin Markech <martin.markech@softpoint.sk>
2025-10-10 14:59:43 +0200

    Fix data pack for invoice

--- lib/pohoda/builders/typ/type_round.rb
+++ lib/pohoda/builders/typ/type_round.rb
@@ -6,14 +6,9 @@ module Pohoda
         include Typ::Groups::MyGroupOfRound
 
         def builder
-          root = Ox::Element.new(name)
-          root = add_attributes_and_namespaces(root)
+          root = super
 
           root << build_element('typ:priceRound', data[:price_round], data[:price_round_attributes]) if data.key? :price_round
-
-          mega.each do |r|
-            r.nodes.each { |n| root << n }
-          end
 
           root
         end
