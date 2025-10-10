95f022f536c9289a22c417d7ca8a5f757353a8ab
Martin Markech <martin.markech@softpoint.sk>
2025-10-10 13:25:59 +0200

    Allow array response for acu:listAccountingUnit in response pack

--- lib/pohoda/parsers/acu/list_accounting_unit_type.rb
+++ lib/pohoda/parsers/acu/list_accounting_unit_type.rb
@@ -4,15 +4,15 @@ module Pohoda
       class ListAccountingUnitType < Rdc::ListVersionType
         include ParserCore::BaseParser
 
-        def item_accounting_unit
-          submodel_at(Acu::ItemAccountingUnitType, 'acu:itemAccountingUnit')
+        def item_accounting_units
+          array_of_at(Acu::ItemAccountingUnitType, ['acu:itemAccountingUnit'])
         end
 
         def to_h
           hash = {}
           hash[:attributes] = attributes
 
-          hash[:item_accounting_unit] = item_accounting_unit.to_h if has? 'acu:itemAccountingUnit'
+          hash[:item_accounting_units] = item_accounting_units.map(&:to_h) if has? 'acu:itemAccountingUnit'
 
           hash
           super.merge(hash)
